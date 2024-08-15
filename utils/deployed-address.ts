7e100ae1e1967e938146e1e4f1127ebe7e93eddf
huongleeexz hung <anhhuongle985@gmail.com>
2024-08-15 15:28:01 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x32B8fa47AabAbF3b651C0f256b02c5056E8c472e'
+export const ProxyAdmin = '0xD6EA2e649eF8E96024CE9470DEED6aA1DC8Bcf12'
+export const SWTRImplementation = '0xB29fd376B09EB87bfCccBDdd0Bf562AbF531A883'
     
